500005ccf68d8a2268837ddc6f70f6e182ac1665
vendavel <sweety@s.com>
2015-08-31 16:22:50 +0100

    Changes to be committed: 	modified:   my_fruitful_functions.py

--- my_fruitful_functions.py
+++ my_fruitful_functions.py
@@ -1,10 +1,12 @@
+from math import *
+
 def compare(x, y):
 	if x > y: return 1
 	elif y > x: return -1
 	else: return 0
 
 def hypotenuse(a,b):
-	temp = a ** 2 + b ** 2
+	temp = sqrt(a ** 2 + b ** 2)
 	print temp
 	return 0.0
 
